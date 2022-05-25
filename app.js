6fcc3b9b0e55a0f03fa92f1b7481893f4ebb0152
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-05-25 11:28:30 -0300

    EJS configurando com o Express

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
+
 const checkListsRouter = require('./src/routes/checkList');
+const rootRouter = require('./src/routes/index');
+
 //Importando a config
 require('./config/database');
+
 
 const app = express();
 app.use(express.json()); // chamando midlware
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 app.use('/checkLists', checkListsRouter);
 
 app.listen(3000, () => {
