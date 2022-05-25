baa05d803cc780565776e69c4f0616b84d9d3e8c
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-05-25 10:59:06 -0300

    Models e as rotas

--- src/routes/checkList.js
+++ src/routes/checkList.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
 const router = express.Router(); // Permite criar rotas em varios arquivos e depois utilizar
+const Checklist = require('../models/checklist'); 
 
 router.get('/', (req, res) => {
   console.log('Olá');
   res.send();
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.status(200).send(req.body);
+router.post('/', async (req, res) => {
+  let { name } = req.body;
+
+  try {
+    let checkList = await Checklist.create({name})
+    res.status(200).send(checkList);
+  } catch (error) {
+    res.status(422).json(error)
+  }
+  // console.log(name);
 })
 
 router.get('/:id', (req,res) => {
